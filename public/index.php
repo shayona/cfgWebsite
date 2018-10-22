2a9dce332805e5fbca74313e3f956cec4d026471
PalashSharma20 <palash.sharma2002@gmail.com>
2018-10-22 22:37:03 +0800

    make it better

--- public/index.php
+++ public/index.php
@@ -23,17 +23,7 @@ if (array_key_exists($page,$pages)) {
 
   if ($page == "projects") {
 
-    $params["projects"] = "";
-
-    if (file_exists("projects.json")) {
-      $params["projects"] = json_decode(file_get_contents("projects.json"),true);
-    }
-
-    if ($params["projects"] == "") {
-      ob_start();
-      require_once 'updateProjects.php';
-      $params["projects"] = json_decode(ob_get_clean(),true);
-    }
+    $params["projects"] = json_decode(file_get_contents("https://script.googleusercontent.com/macros/echo?user_content_key=E1icTPJPDXCJsOPTKHSK_0SdD-DQ7CfI0zQUa_9PAuigOz9BMmmbpZUwUK3cq4I1caNyqq1mt7EnRzMGwRU6D6RjcKkXWw1_m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnJK4DgJ2qkfav5JQj27i5FfvFF4kxUm2jiUROOUi_SO2e9Gfj4XcOcBxtIS9z7MUuHSTNzgsg0p2&lib=MlwJGesRDLu0emjvioR11_UAcCTvbymtO"), true);
 
     if (isset($_GET['id'])) {
 
