182fe4a84943703e7a8c6876e100970e2fc1035d
PalashSharma20 <palash.sharma2002@gmail.com>
2018-10-30 16:15:09 +0800

    make it better

--- public/index.php
+++ public/index.php
@@ -18,11 +18,11 @@ set_exception_handler('Core\Error::exceptionHandler');
 $router = new Core\Router();
 
 $router->add('', ['controller' => 'pages', 'action' => 'show-page', 'page' => 'home']);
-$router->add('contact', ['controller' => 'pages', 'action' => 'show-page', 'page' => 'contact']);
-$router->add('hackathon', ['controller' => 'pages', 'action' => 'show-page', 'page' => 'hackathon']);
 
 $router->add('projects', ['controller' => 'projects', 'action' => "all-projects", "page" => "projects"]);
 $router->add('projects/update', ['controller' => 'projects', 'action' => "update"]);
 $router->add('projects/{id}', ['controller' => 'projects', 'action' => "show-project", "page" => "projects"]);
+
+$router->add('{page}', ['controller' => 'pages', 'action' => 'show-page']);
 
 $router->dispatch($_SERVER['QUERY_STRING']);
